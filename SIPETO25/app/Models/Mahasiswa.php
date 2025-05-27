c69a1a034f3a10c71e00114b0d2fc8886a1fa977
dhevinaagustina <dhevinaagustina@gmail.com>
2025-05-27 12:52:04 +0700

    Perbaikan Error

--- SIPETO25/app/Models/Mahasiswa.php
+++ SIPETO25/app/Models/Mahasiswa.php
@@ -28,12 +28,11 @@ class Mahasiswa extends Authenticatable
     {
         return $this->hasMany(Message::class, 'mahasiswa_id', 'id_mahasiswa');
     }
+    
+    public function pendaftaranToeic()
+    {
+        return $this->hasOne(PendaftaranToeic::class, 'id_mahasiswa');
+    }
 }
 
 
-
-public function pendaftaranToeic()
-{
-    return $this->hasOne(PendaftaranToeic::class, 'id_mahasiswa');
-}
-}
