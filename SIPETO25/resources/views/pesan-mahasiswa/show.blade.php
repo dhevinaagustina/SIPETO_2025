ee3f26b9ed5241f603f3958290b31be60575900c
dhevinaagustina <dhevinaagustina@gmail.com>
2025-06-02 13:45:41 +0700

    pesan mahasiswa

--- SIPETO25/resources/views/pesan-mahasiswa/show.blade.php
+++ SIPETO25/resources/views/pesan-mahasiswa/show.blade.php
@@ -6,15 +6,47 @@
         <h5>{{ $pesan->judul }}</h5>
     </div>
     <div class="card-body">
+        {{-- Isi pesan --}}
         <p>{!! nl2br(e($pesan->isi)) !!}</p>
 
-        @if($pesan->tipe_lampiran === 'link')
-            <p><a href="{{ $pesan->lampiran }}" target="_blank" class="btn btn-info">🔗 Kunjungi Link</a></p>
-        @elseif(in_array($pesan->tipe_lampiran, ['dokumen', 'gambar']))
-            <p><a href="{{ asset('storage/' . $pesan->lampiran) }}" target="_blank" class="btn btn-primary">📎 Unduh Lampiran</a></p>
-            @if($pesan->tipe_lampiran === 'gambar')
-                <img src="{{ asset('storage/' . $pesan->lampiran) }}" class="img-fluid mt-3" alt="Lampiran Gambar">
-            @endif
+        {{-- Tampilkan Lampiran --}}
+        @if($pesan->lampiran)
+            <div class="mt-3">
+                <strong>Lampiran:</strong><br>
+
+                @php
+                    $ext = strtolower(pathinfo($pesan->lampiran, PATHINFO_EXTENSION));
+                    $filePath = 'storage/' . $pesan->lampiran;
+                @endphp
+
+                {{-- Jika tipe link --}}
+                @if($pesan->tipe_lampiran === 'link')
+                    <a href="{{ $pesan->lampiran }}" target="_blank" class="btn btn-info">
+                        🔗 Kunjungi Link
+                    </a>
+
+                {{-- Jika file lokal --}}
+                @else
+                    {{-- Tombol unduh --}}
+                    <a href="{{ route('pesan.download', $pesan->id) }}" class="btn btn-primary">
+                        📎 Unduh Lampiran
+                    </a>
+
+                    {{-- Preview Gambar --}}
+                    @if(in_array($ext, ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'webp']))
+                        <div class="mt-3">
+                            <img src="{{ asset($filePath) }}" class="img-fluid rounded shadow-sm" alt="Lampiran Gambar">
+                        </div>
+                    @endif
+
+                    {{-- Preview PDF --}}
+                    @if($ext === 'pdf')
+                        <div class="mt-3">
+                            <iframe src="{{ asset($filePath) }}" width="100%" height="500px"></iframe>
+                        </div>
+                    @endif
+                @endif
+            </div>
         @endif
     </div>
 </div>
